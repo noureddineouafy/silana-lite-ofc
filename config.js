1693198550a629dabda1aadab69338fdca9ce085
Alsoltanahmed1 <ahmedmohmed200520055@gmail.com>
2025-07-02 03:51:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url'
 
 // Owner
 global.owner = [
-['212717457920', 'Noureddine', true]
+['201140627942', 'ALSOLTAN AHMED', true]
 ]
 global.mods = []
 global.prems = []
