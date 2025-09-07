bb645d7070248daaddb76074fafe41099fb31c38
noureddineouafy <130229691+noureddineouafy@users.noreply.github.com>
2025-09-08 00:13:09 +0100

    Create listplugins.js

---
+++ plugins/listplugins.js
@@ -0,0 +1,18 @@
+// instagram.com/noureddine_ouafy
+import fs from "fs";
+
+let handler = async (m, { conn }) => {
+  let dir = fs.readdirSync("./plugins");
+  if (dir.length < 1) return m.reply("No plugin files found.");
+
+  let teks = "\n";
+  for (let e of dir) {
+    teks += `* ${e}\n`;
+  }
+  m.reply(teks);
+};
+
+handler.command = ["listplugins"];
+handler.help = ["listplugins"];
+handler.tags= ["infobot"];
+export default handler;
