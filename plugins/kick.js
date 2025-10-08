74fbf0a1197c9ce4984c6f75284e30aca1f75ac8
noureddineouafy <130229691+noureddineouafy@users.noreply.github.com>
2025-10-08 13:21:13 +0100

    Update kick.js

--- plugins/kick.js
+++ plugins/kick.js
@@ -1,49 +1,18 @@
-import { areJidsSameUser } from "@adiwajshing/baileys";
 let handler = async (m, { conn, text, participants }) => {
-  let users;
-  if (m.quoted) {
-    users = m.quoted.sender;
-  } else if (m.mentionedJid[0]) {
-    users = m.mentionedJid.filter((u) => !areJidsSameUser(u, conn.user.id));
-  } else if (!text) return m.reply("reply/tag member");
+    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : text ? (text.replace(/\D/g, '') + '@s.whatsapp.net') : ''
+    if (!who || who == m.sender) throw 'Reply/tag member'
+    if (participants.filter(v => v.jid == who).length == 0) throw `The target not in this group!`
+    conn.groupParticipantsUpdate(m.chat, [who], 'remove')
+        .then(_ => m.reply(`Success`))
+}
 
-  let kickedUser = [];
-  if (m.quoted) {
-    await conn.groupParticipantsUpdate(m.chat, [users], "remove");
-  } else {
-    for (let user of users) {
-      if (
-        user.endsWith("@s.whatsapp.net") &&
-        !(
-          participants.find((v) => areJidsSameUser(v.id, user)) || {
-            admin: true,
-          }
-        ).admin
-      ) {
-        const res = await conn.groupParticipantsUpdate(
-          m.chat,
-          [user],
-          "remove",
-        );
-        kickedUser.concat(res);
-        await delay(1 * 1000);
-      }
-    }
-  }
-  m.reply(
-    `*Successful Issue* ${m.quoted ? m.quoted.sender.split("@")[0] : kickedUser.map((v) => "@" + v.split("@")[0])}`,
-    null,
-    { mentions: kickedUser },
-  );
-};
-handler.help = ["kick"];
-handler.tags = ["owner"];
-handler.command = /^(kick)$/i;
+handler.help = ['kick']
+handler.tags = ['owner']
+handler.command = /^(kick)$/i
 
-handler.admin = true;
-handler.group = true;
-handler.botAdmin = true;
+handler.owner = true 
+handler.group = true
+handler.botAdmin = true
+handler.admin = true
 
-export default handler;
-
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+export default handler
